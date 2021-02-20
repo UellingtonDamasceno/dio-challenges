ac5a0a60c42413554e427fad5263eefbac3e6400
Uellington Damasceno <uellington99@gmail.com>
2021-02-20 03:43:21 -0300

    Small improvements.

--- java/blog-post-text-reducer/src/main/java/blog/post/text/reducer/Main.java
+++ java/blog-post-text-reducer/src/main/java/blog/post/text/reducer/Main.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.mapping;
@@ -25,19 +24,22 @@ import static java.util.stream.Collectors.toMap;
  */
 public class Main {
 
-   public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
-        List<String> wordsPhrase = new LinkedList();
+        final List<String> wordsPhrase = new LinkedList();
+        final List<String> biggestWords = new ArrayList(26);
 
-        Function<List<String>, Map<String, Long>> countRecurrence = words -> words
+        Function<Map.Entry<String, List<String>>, Map<String, Long>> countRecurrence = entry -> entry
+                .getValue()
                 .stream()
                 .collect(toMap(Function.identity(), word -> wordsPhrase.stream()
                 .filter(currentWord -> currentWord.equals(word))
                 .count()));
 
         for (String line = br.readLine(); !line.equals("."); line = br.readLine()) {
-            wordsPhrase.addAll(Arrays.asList(line.trim().split(" ")));
-            HashMap<String, List<String>> dictionary = wordsPhrase.stream()
+            wordsPhrase.addAll(Arrays.asList(line.split(" ")));
+
+            Map<String, List<String>> dictionary = wordsPhrase.stream()
                     .distinct()
                     .filter(word -> word.length() > 2)
                     .collect(groupingBy(
@@ -45,44 +47,43 @@ public class Main {
                             HashMap::new,
                             mapping(Function.identity(), toList())));
 
-            List<Map.Entry<String, Long>> recurrenceTable = dictionary.entrySet()
+            List<String> recurrenceTable = dictionary.entrySet()
                     .parallelStream()
-                    .map(entry -> countRecurrence.apply(entry.getValue()))
+                    .map(countRecurrence)
                     .flatMap(map -> map.entrySet()
                     .stream()
                     .map(entry -> Map.entry(entry.getKey(), entry.getValue() * (entry.getKey().length() - 2))))
                     .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                    .map(Entry::getKey)
                     .collect(toList());
 
-            List<Map.Entry<String, Long>> orderedWords = dictionary.entrySet()
+            dictionary.entrySet()
                     .stream()
-                    .map(entry -> entry.getKey())
+                    .map(Entry::getKey)
                     .map(character -> recurrenceTable.stream()
-                    .filter(entry -> entry.getKey().startsWith(character))
+                    .filter(word -> word.startsWith(character))
                     .findFirst().get())
-                    .sorted(Map.Entry.comparingByKey())
-                    .collect(toList());
+                    .sorted()
+                    .collect(() -> biggestWords, List::add, List::addAll);
 
             String phrase = wordsPhrase.stream().collect(joining(" "));
-            List<String> biggestWords = orderedWords.stream().map(entry -> entry.getKey()).collect(toList());
-            phrase = replace(biggestWords, phrase);
 
-            System.out.println(phrase);
-            System.out.println(orderedWords.size());
-            orderedWords.forEach(entry -> {
-                String word = entry.getKey();
+            System.out.println(replace(biggestWords, phrase, 0));
+            System.out.println(biggestWords.size());
+            biggestWords.forEach(word -> {
                 System.out.println(word.charAt(0) + ". = " + word);
             });
+            biggestWords.clear();
             wordsPhrase.clear();
         }
     }
 
-    private static String replace(List<String> wordsPhrase, String phrase) {
-        if (wordsPhrase.isEmpty()) {
+    private static String replace(List<String> wordsPhrase, String phrase, int index) {
+        if (wordsPhrase.size() == index) {
             return phrase;
         }
-        String word = wordsPhrase.remove(0);
+        String word = wordsPhrase.get(index);
         phrase = phrase.replaceAll("\\b" + word + "\\b", word.charAt(0) + ".");
-        return replace(wordsPhrase, phrase);
+        return replace(wordsPhrase, phrase, ++index);
     }
 }
