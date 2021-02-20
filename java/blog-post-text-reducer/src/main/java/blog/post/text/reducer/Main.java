4a772b303068eb05339e49855c9d8e091fc34ef4
Uellington Damasceno <uellington99@gmail.com>
2021-02-20 00:55:06 -0300

    Recursive replace.

--- java/blog-post-text-reducer/src/main/java/blog/post/text/reducer/Main.java
+++ java/blog-post-text-reducer/src/main/java/blog/post/text/reducer/Main.java
@@ -3,19 +3,21 @@ package blog.post.text.reducer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.function.Supplier;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
 
 /**
  *
@@ -23,41 +25,64 @@ import static java.util.stream.Collectors.toSet;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
+        List<String> wordsPhrase = new LinkedList();
 
-        String abbreviationPhrase;
-        String biggestWord;
+        Function<List<String>, Map<String, Long>> countRecurrence = words -> words
+                .stream()
+                .collect(toMap(Function.identity(), word -> wordsPhrase.stream()
+                .filter(currentWord -> currentWord.equals(word))
+                .count()));
 
         for (String line = br.readLine(); !line.equals("."); line = br.readLine()) {
-            List<String> phrase = Arrays.asList(line.trim().split(" "));
-            Set<String> filteredWords = phrase.stream().collect(toSet());
+            wordsPhrase.addAll(Arrays.asList(line.trim().split(" ")));
+            HashMap<String, List<String>> dictionary = wordsPhrase.stream()
+                    .distinct()
+                    .filter(word -> word.length() > 2)
+                    .collect(groupingBy(
+                            word -> word.substring(0, 1),
+                            HashMap::new,
+                            mapping(Function.identity(), toList())));
 
-            Function<String, Long> countRecurrence = word -> phrase.stream()
-                    .filter(currentWord -> currentWord.equals(word))
-                    .count();
-
-            Map<String, Long> recurrenceTable = filteredWords.stream()
-                    .collect(toMap(Function.identity(), countRecurrence));
-
-            List<Entry<String, Long>> orderedWords = recurrenceTable.entrySet()
+            List<Map.Entry<String, Long>> recurrenceTable = dictionary.entrySet()
+                    .parallelStream()
+                    .map(entry -> countRecurrence.apply(entry.getValue()))
+                    .flatMap(map -> map.entrySet()
                     .stream()
-                    .map((entry) -> Map.entry(entry.getKey(), entry.getKey().length() * entry.getValue()))
+                    .map(entry -> Map.entry(entry.getKey(), entry.getValue() * (entry.getKey().length() - 2))))
                     .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                     .collect(toList());
 
-            biggestWord = orderedWords.get(0).getKey();
+            List<Map.Entry<String, Long>> orderedWords = dictionary.entrySet()
+                    .stream()
+                    .map(entry -> entry.getKey())
+                    .map(character -> recurrenceTable.stream()
+                    .filter(entry -> entry.getKey().startsWith(character))
+                    .findFirst().get())
+                    .sorted(Map.Entry.comparingByKey())
+                    .collect(toList());
 
-            String abbreviation = (biggestWord.length() > 2) ? String.format("%s.", biggestWord.charAt(0)) : biggestWord;
-            abbreviationPhrase = (!abbreviation.equals(biggestWord))
-                    ? phrase.stream().collect(Collectors.joining(" ")).replaceAll(biggestWord, abbreviation)
-                    : phrase.stream().collect(Collectors.joining(" "));
+            String phrase = wordsPhrase.stream().collect(joining(" "));
+            List<String> biggestWords = orderedWords.stream().map(entry -> entry.getKey()).collect(toList());
+            phrase = replace(biggestWords, phrase);
 
-            System.out.println(abbreviationPhrase);
-            System.out.println(recurrenceTable.get(biggestWord));
-            System.out.println(abbreviation + " = " + biggestWord);
-
+            System.out.println(phrase);
+            System.out.println(orderedWords.size());
+            orderedWords.forEach(entry -> {
+                String word = entry.getKey();
+                System.out.println(word.charAt(0) + ". = " + word);
+            });
+            wordsPhrase.clear();
         }
     }
 
+    private static String replace(List<String> wordsPhrase, String phrase) {
+        if (wordsPhrase.isEmpty()) {
+            return phrase;
+        }
+        String word = wordsPhrase.remove(0);
+        phrase = phrase.replaceAll("\\b" + word + "\\b", word.charAt(0) + ".");
+        return replace(wordsPhrase, phrase);
+    }
 }
